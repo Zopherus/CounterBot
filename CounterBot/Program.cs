f52d1626c112aa533da08bfd9d6c1b6d6a5ba922
Eric Zhu <ejz1999@yahoo.com>
2016-05-22 01:59:30 -0700

    Installed Selenium Webdriver

---
+++ CounterBot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CounterBot
+{
+    class Program
+    {
+        private static IWebDriver driver = new ChromeDriver(Directory.GetCurrentDirectory());
+
+        static void Main(string[] args)
+        {
+            driver.Navigate().GoToUrl("https://www.messenger.com");
+
+            IWebElement lastMessage = driver.FindElement(By.Id("js_2")).FindElements(By.TagName("div")).Last();
+        }
+    }
+}
